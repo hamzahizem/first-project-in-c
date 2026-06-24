d996e1ca4a5a04238b7055606b2a0e64e490e330
hamzahizem <159248116+hamzahizem@users.noreply.github.com>
2026-06-24 16:42:41 +0100

    Add files via upload
    Migrate from fork() to single-process poll() for I/O multiplexing
    
    - Completely removed fork() and mmap() shared memory architecture.
    - Implemented poll() loop to handle multiple client sockets in a single process.
    - Fixed a buffer bug by using raw byte counts instead of strlen() during forward.
    - Enabled seamless client reconnection by managing dynamic fd slot assignments.

--- main.c
+++ main.c
@@ -6,70 +6,94 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <signal.h>
+#include <time.h>
+#include <sys/mman.h>
+#include <poll.h>
 void error(const char *msg){
     perror(msg);
-    exit(1);
-}
-int main(int argc,char *argv[])
-{
+    exit(1);}
 
+int main(int argc,char *argv[]){
     if(argc < 2){
         fprintf(stderr ,"port is not provided , pls write the port pls.\n");
         exit(1);}
-    int sockfd , newsockfd , portno,n,t=1;
+    int sockfd , newsockfd , portno,n=1,sockfdto,x=1,i=0;
     char buffer[255],str[255];
-    struct sockaddr_in serv_addr , cli_addr;
+    struct sockaddr_in serv_addr , cli_addr,clin_addr;
     socklen_t clilen;
+    socklen_t clilen2;
     sockfd = socket(AF_INET,SOCK_STREAM,0);
     if(sockfd<0){
         error("Error oprning Socket");}
     bzero((char *) &serv_addr ,sizeof(serv_addr));
     portno=atoi(argv[1]);
-
     serv_addr.sin_family=AF_INET;
     serv_addr.sin_addr.s_addr= INADDR_ANY;
     serv_addr.sin_port=htons(portno);
     if(bind(sockfd,(struct sockaddr *) & serv_addr ,sizeof(serv_addr))<0){
-        error("Binding Failed.");
-    }
+        error("Binding Failed.");}
     listen(sockfd,5);
-    clilen=sizeof(cli_addr);
-    newsockfd =accept(sockfd,(struct sockaddr *) &cli_addr,&clilen);
-    if (newsockfd<0){
-        error("error on accepting the conection");
-    }
-    pid_t pid = fork();
-    if (pid>0){
-        while(t){
-        bzero(buffer,255);
-        n = recv(newsockfd,buffer,255,0);
-        if (n<0){
-            error("error in reading");}
-
-        if(n==0){
-            printf("the Client has disconected from server.\n");
-            kill(pid,SIGTERM);
-            t=0;
+    struct pollfd fds[3];
+    fds[0].fd = sockfd;
+    fds[0].events = POLLIN;
+    fds[1].fd = -1;
+    fds[2].fd = -1;
+    while(1){
+        int poll1=poll(fds,3,-1);
+        if(poll1<0){
+            perror("Accept Failed");}
+        if(fds[0].revents & POLLIN && fds[1].fd==-1){
+            clilen=sizeof(cli_addr);
+            newsockfd =accept(sockfd,(struct sockaddr *) &cli_addr,&clilen);
+            fds[1].fd=newsockfd;
+            fds[1].events =POLLIN;
+            i++;
+            if(newsockfd<0){
+                error("accept failed");}}
+        else if(fds[0].revents & POLLIN && fds[1].fd!=-1){
+            clilen2=sizeof(clin_addr);
+            sockfdto =accept(sockfd,(struct sockaddr *) &clin_addr,&clilen2);
+            fds[2].fd=sockfdto;
+            fds[2].events=POLLIN;
+            i++;
+            if(sockfdto<0){
+                error("accept failed");}}
+        if(fds[1].fd != -1 && (fds[1].revents & POLLIN)){
+            bzero(buffer, sizeof(buffer));
+            int n = recv(fds[1].fd, buffer, sizeof(buffer), 0);
+            if (n <= 0) {
+                printf("Client 1 disconnected.\n");
+                close(fds[1].fd);
+                fds[1].fd = -1;
+                i--;
+            } else if (fds[2].fd != -1) {
+                send(fds[2].fd, buffer, n, 0);}}
+        if (fds[2].fd != -1 && (fds[2].revents & POLLIN)) {
+            bzero(buffer, sizeof(buffer));
+            int n = recv(fds[2].fd, buffer, sizeof(buffer), 0);
+            if (n <= 0) {
+                printf("Client 2 disconnected.\n");
+                close(fds[2].fd);
+                fds[2].fd = -1;
+                i--;
+            } else if (fds[1].fd != -1) {
+                send(fds[1].fd, buffer, n, 0);
+            }
         }
-        if(n>0){
-        printf("Client :%s\n",buffer);
-        }
-    }}
-    if (pid==0){
-        while(1){
-        bzero(str,255);
-        fgets(str,255,stdin);
-        n=send(newsockfd,str,strlen(str)-1,0);
-        if(n<0){
-            error("error while writeing.");}
+
 
         }
-
-    }
-
 
 
     close(newsockfd);
+    close(sockfdto);
     close(sockfd);
-    return 0;
-}
+    return 0;}
+
+
+
+
+
+
+
+
